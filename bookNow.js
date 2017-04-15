cf95eb1719f36317cfdc876e3ee48bb10f561d89
Dorota Buraczyńska <dorota.buraczynska@gmail.com>
2017-04-15 13:32:03 +0200

    you can choose date and hour of meeting

--- bookNow.js
+++ bookNow.js
@@ -25,7 +25,65 @@ $('#calendar').datepicker({
 
 });
 
+$('.appointment--book-now').on('click', function () {
+    $('.container--book-now').css('display', 'none');
+    $('.form-container').css('display', 'none');
+    $('.container--schedule').css('display', 'block');
+});
 
+$('.back').on('click', function () {
+    $('.container--schedule').css('display', 'none');
+    $('.container--book-now').css('display', 'block');
+});
 
+$('.weekly-view').on('click', function () {
+    $('.ui-datepicker tr:first-child').siblings().toggleClass('invisible');
+    var text = $('.weekly-view').text();
+    if (text == "Weekly view") {
+        $('.weekly-view').text('Monthly view');
+        $('.weekly-view-container i')
+            .removeClass('fa-angle-up')
+            .addClass('fa-angle-down');
+    } else {
+        $('.weekly-view').text('Weekly view');
+        $('.weekly-view-container i')
+            .removeClass('fa-angle-down')
+            .addClass('fa-angle-up');
+    }
+});
+console.log($('#calendar a.ui-state-default'));
+$('a.ui-state-default').on('click', function () {
+    $('.appointment-hours').css('display', 'block');
+    var month = $('.ui-datepicker-month').text();
+    var day = $(this).text();
+    console.log(day);
+    var year = $('.ui-datepicker-year').text();
+    $('.last-step-month').text(month);
+    $('.last-step-day').text(day + ", ");
+    $('.last-step-year').text(year);
+});
 
-//$('.appointment--book-now')
+$('.ui-datepicker-week-end').on('click', function () {
+    $('.no-available-hours').css('display', 'block');
+    $('.appointment-hours').css('display', 'none');
+});
+
+$('#book-now-page').on('click', '.appointment--book-now', function () {
+    var offerNum = $(this.closest('.offers')).index();
+    var offerTitle = $(".container--book-now .offer").eq(offerNum - 1).text();
+    var offerPrice = $(".container--book-now .price").eq(offerNum - 1).text();
+    $('.last-step-title').text(offerTitle);
+    $('.last-step-price').text(offerPrice);
+
+});
+
+$('.appointment-hours .hour').on('click', function () {
+    var hour = $(this).text();
+    $('.last-step-hour').text(hour);
+});
+
+$('.appointment--last-step').on('click', function () {
+
+    $('.form-container').css('display', 'block');
+    $('.container--schedule').css('display', 'none');
+});
