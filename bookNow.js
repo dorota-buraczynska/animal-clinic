f5c950e953189e46a73cf7f3b9dc536e11d0f208
Dorota Buraczyńska <dorota.buraczynska@gmail.com>
2017-04-15 22:26:10 +0200

    do corrections in schedule page and form page

--- bookNow.js
+++ bookNow.js
@@ -19,6 +19,9 @@ $('#calendar').datepicker({
     prevText: "<",
     nextText: ">",
     inline: true,
+    onSelect: function (date, inst) {
+        pickDate(new Date(date));
+    },
     firstDay: 1,
     showOtherMonths: true,
     dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
@@ -30,6 +33,7 @@ $('.appointment--book-now').on('click', function () {
     $('.section-title-wrapper').css('display', 'block');
     $('.container--schedule').css('display', 'block');
     $('.form-container').css('display', 'none');
+    $('.last-step').addClass('disabled');
 });
 
 // back from schedule page to book-now-page
@@ -58,20 +62,19 @@ $('.weekly-view').on('click', function () {
     }
 });
 
-// show month, day, year and time of chose appointment
-console.log($('#calendar a.ui-state-default'));
-$('a.ui-state-default').on('click', function () {
+var formatDate = function (date) {
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var month = date.getMonth();
+    var day = date.getDate();
+    var year = date.getFullYear();
+    return months[month] + " " + day + ", " + year;
+};
+
+
+var pickDate = function (date) {
     $('.appointment-hours').css('display', 'block');
-    var month = $('.ui-datepicker-month').text();
-    var day = $(this).text();
-    console.log(day);
-    var year = $('.ui-datepicker-year').text();
-    $('.last-step-month').text(month);
-    $('.last-step-day').text(day + ", ");
-    $('.last-step-year').text(year);
-});
-
-
+    $('.last-step-date').text(formatDate(date));
+};
 
 //shows hours in weekend
 $('.ui-datepicker-week-end').on('click', function () {
@@ -93,6 +96,10 @@ $('#book-now-page').on('click', '.appointment--book-now', function () {
 $('.appointment-hours .hour').on('click', function () {
     var hour = $(this).text();
     $('.last-step-hour').text(hour);
+    $('.last-step').removeClass('disabled');
+    $('.appointment--last-step').removeClass('disabled');
+
+
 });
 
 //switch schedule page to form page
@@ -100,18 +107,18 @@ $('.appointment--last-step').on('click', function () {
     $('.form-container').css('display', 'block');
     $('.container--schedule').css('display', 'none');
     formLastStep();
+    $('.form-last-step').addClass('disabled');
 });
 
+//
 var formLastStep = function () {
     var title = $('.last-step-title').text();
     var price = $('.last-step-price').text();
-    var month = $('.last-step-month').text();
-    var day = $('.last-step-day').text();
+    var date = $('.last-step-date').text();
     var hour = $('.last-step-hour').text();
     $('.form-last-step-title').text(title);
     $('.form-last-step-price').text(price);
-    $('.form-last-step-month').text(month);
-    $('.form-last-step-day').text(day);
+    $('.form-last-step-date').text(date);
     $('.form-last-step-hour').text(hour);
 };
 
